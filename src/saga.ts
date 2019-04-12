24f0fa2c651d2fd177c6689d4c71e7850e3aed07
Masterchief <live.abhishek@outlook.com>
2019-04-12 16:53:19 +0530

    Added Sagas. Updated store.ts.

--- src/saga.ts
+++ src/saga.ts
@@ -1,3 +1,20 @@
+import { all, delay, put, takeEvery } from "redux-saga/effects";
+
 export function* helloSaga(): any {
   console.log("Hello Sagas!");
+}
+
+// This saga will perform Async increment task
+export function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: "INCREMENT" });
+}
+
+// Spawn new Increment Async task on each INCREMENT_ASYNC
+export function* watchIncrementAsync() {
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
+}
+
+export default function* rootSaga() {
+  yield all([helloSaga(), watchIncrementAsync()]);
 }
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 import reducer from "./reducers";
-import { helloSaga } from "./saga";
+import rootSaga from "./saga";
 
 const sagaMiddleWare = createSagaMiddleware();
 export const store = createStore(reducer, applyMiddleware(sagaMiddleWare));
-sagaMiddleWare.run(helloSaga);
+sagaMiddleWare.run(rootSaga);
