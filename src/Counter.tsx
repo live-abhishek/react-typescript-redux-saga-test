eae3323bc86343e7da2fa3eb37e5f7eb66737770
Masterchief <live.abhishek@outlook.com>
2019-04-12 16:16:38 +0530

    Added an explicit mapDispatchToProps sample.

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Dispatch } from "redux";
 
 interface StateProps {
   value: number;
@@ -22,6 +23,12 @@ const Counter = (props: StateProps & DispatchProps) => {
   );
 };
 const mapStateToProps = (state: number): StateProps => ({ value: state });
+// * this is explicit way of using dispatch;
+const mapDispatchToPropsExplicit = (dispatch: Dispatch<{ type: string }>) => ({
+  onIncrement: () => dispatch({ type: "INCREMENT" }),
+  onDecrement: () => dispatch({ type: "DECREMENT" })
+});
+
 const mapDispatchToProps = {
   onIncrement: () => ({ type: "INCREMENT" }),
   onDecrement: () => ({ type: "DECREMENT" })
