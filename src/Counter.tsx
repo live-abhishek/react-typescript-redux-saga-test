8dc9c91a862e98dfd745ca1b9e36707b0054c887
Masterchief <live.abhishek@outlook.com>
2019-04-12 16:52:08 +0530

    Updated Counter UI.

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -9,12 +9,14 @@ interface StateProps {
 interface DispatchProps {
   onIncrement: () => void;
   onDecrement: () => void;
+  onIncrementAsync: () => void;
 }
 
 const Counter = (props: StateProps & DispatchProps) => {
-  const { value, onIncrement, onDecrement } = props;
+  const { value, onIncrement, onDecrement, onIncrementAsync } = props;
   return (
     <div>
+      <button onClick={onIncrementAsync}>Increment after 1 second</button>{" "}
       <button onClick={onIncrement}>Increment</button>{" "}
       <button onClick={onDecrement}>Decrement</button>
       <hr />
@@ -26,12 +28,14 @@ const mapStateToProps = (state: number): StateProps => ({ value: state });
 // * this is explicit way of using dispatch;
 const mapDispatchToPropsExplicit = (dispatch: Dispatch<{ type: string }>) => ({
   onIncrement: () => dispatch({ type: "INCREMENT" }),
-  onDecrement: () => dispatch({ type: "DECREMENT" })
+  onDecrement: () => dispatch({ type: "DECREMENT" }),
+  onIncrementAsync: () => dispatch({ type: "INCREMENT_ASYNC" })
 });
 
 const mapDispatchToProps = {
   onIncrement: () => ({ type: "INCREMENT" }),
-  onDecrement: () => ({ type: "DECREMENT" })
+  onDecrement: () => ({ type: "DECREMENT" }),
+  onIncrementAsync: () => ({ type: "INCREMENT_ASYNC" })
 };
 export default connect<StateProps, DispatchProps, {}, number>(
   mapStateToProps,
